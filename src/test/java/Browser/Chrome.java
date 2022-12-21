d0c58320a95006fdd8cf3dd005f2b9d1b7e8b0cd
cristhain <crisslpj5@gmail.com>
2022-12-20 21:45:56 -0400

    Adding chromdriver bonnyGarcia

--- src/test/java/Browser/Chrome.java
+++ src/test/java/Browser/Chrome.java
@@ -1,15 +1,15 @@
 package Browser;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class Chrome implements IBrowser{
+public class Chrome implements IBrowser {
     @Override
     public WebDriver create() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
-
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
